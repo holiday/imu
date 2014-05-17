78062f51c5f2eaafb77ca64028785e3731c90d9f
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-05-17 03:50:07 -0400

    tidying up code

--- GUI/script.js
+++ GUI/script.js
@@ -1,13 +1,13 @@
-var init = function() {
+var init = function () {
 
     /* List serial devices */
     var select, option, device, conId, disconnected, stringReceived, cube1, cube2, cube3, cube4;
     select = document.getElementById('devices');
     disconnected = document.getElementById('disconnect');
 
-    chrome.serial.getDevices(function(devices) {
-        
-        devices.each(function(port) {
+    chrome.serial.getDevices(function (devices) {
+
+        devices.each(function (port) {
             option = document.createElement("option");
             option.value = port.path;
             option.text = port.path;
@@ -16,36 +16,40 @@ var init = function() {
     });
 
     /*Select serial device change */
-    select.onchange = function(e) {
+    select.onchange = function (e) {
         device = select.options[select.selectedIndex].value;
-        chrome.serial.connect(device, {bitrate: 115200, dataBits: "eight", parityBit: "no", stopBits: "one"}, onConnect)
-        
+        chrome.serial.connect(device, {
+            bitrate: 115200,
+            dataBits: "eight",
+            parityBit: "no",
+            stopBits: "one"
+        }, onConnect);
     }
 
-    var onConnect = function(connectionInfo) {
+    var onConnect = function (connectionInfo) {
         console.log("connected");
         conId = connectionInfo.connectionId;
         disconnected.disabled = false;
 
-        disconnected.addEventListener("click", function(e) {
+        disconnected.addEventListener("click", function (e) {
             e.preventDefault();
-            chrome.serial.disconnect(conId, function(result){
-                if(result){
+            chrome.serial.disconnect(conId, function (result) {
+                if (result) {
                     disconnected.disabled = true;
                     console.log("disconnected");
                 }
             });
-        },false);
+        }, false);
 
         //listen for data
         chrome.serial.onReceive.addListener(onReceiveCallback);
-        
+
     }
 
-    var ab2str = function(buf) {
-      var bufView = new Uint8Array(buf);
-      var encodedString = String.fromCharCode.apply(null, bufView);
-      return decodeURIComponent(escape(encodedString));
+    var ab2str = function (buf) {
+        var bufView = new Uint8Array(buf);
+        var encodedString = String.fromCharCode.apply(null, bufView);
+        return decodeURIComponent(escape(encodedString));
     };
 
 
@@ -55,26 +59,25 @@ var init = function() {
     cube3 = document.getElementById("cube-3");
     cube4 = document.getElementById("cube-4");
 
-    var onReceiveCallback = function(info) {
+    var onReceiveCallback = function (info) {
         if (info.connectionId == conId && info.data) {
             var str = ab2str(info.data);
             var data;
-            if(str.charAt(str.length-1) === '\n'){
-                stringReceived += str.substring(0, str.length-1);
+            if (str.charAt(str.length - 1) === '\n') {
+                stringReceived += str.substring(0, str.length - 1);
                 data = stringReceived.split(",");
                 cube1.style.webkitTransform = "rotateX(" + (-data[0] - 180) + "deg) rotateZ(" + (data[1] - 180) + "deg)";
                 cube2.style.webkitTransform = "rotateX(" + (-data[2] - 180) + "deg) rotateZ(" + (data[3] - 180) + "deg)";
                 cube3.style.webkitTransform = "rotateX(" + (-data[4] - 180) + "deg) rotateZ(" + (data[5] - 180) + "deg)";
                 cube4.style.webkitTransform = "rotateX(" + (-data[6] - 180) + "deg) rotateZ(" + (data[7] - 180) + "deg)";
-                
-                //console.log("x: ", xy[4], "y: ", xy[5]);
+
                 stringReceived = '';
-            }else {
+            } else {
                 stringReceived += str;
             }
         }
     }
-  
+
 };
-  
-window.addEventListener( 'DOMContentLoaded', init, false);
+
+window.addEventListener('DOMContentLoaded', init, false);
